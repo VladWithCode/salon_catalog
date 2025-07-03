53ada65e0e740ac19ebb1ca7f10554195bdb4e7c
vladwb <vladwithcode@gmail.com>
2025-07-03 17:59:42 -0600

    chore: update index page

--- internal/templates/pages/index_templ.go
+++ internal/templates/pages/index_templ.go
@@ -43,7 +43,7 @@ func Index() templ.Component {
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1 class=\"text-4xl font-bold\">Hello, world!</h1>")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<main class=\"px-2\"><h1 class=\"text-4xl font-bold\">Hello, world!</h1></main>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
