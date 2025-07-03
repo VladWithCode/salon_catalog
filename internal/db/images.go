d558458f15885354f3db433b734df10efdfbd4bd
vladwb <vladwithcode@gmail.com>
2025-07-03 17:52:48 -0600

    chore: update Image model

--- internal/db/images.go
+++ internal/db/images.go
@@ -6,14 +6,11 @@ import (
 )
 
 type Image struct {
-	ID           string `db:"id" json:"id"`
-	PublicURL    string `db:"public_url" json:"publicUrl"`
-	InternalPath string `db:"internal_path" json:"internalPath"`
-	NoOptimize   bool   `db:"no_optimize" json:"noOptimize"`
-	Size         int    `db:"size" json:"size"`
-	Group        string `db:"group" json:"group"`
-	Tags         string `db:"tags" json:"tags"`
-	CreatedAt    string `db:"created_at" json:"createdAt"`
+	ID         string    `db:"id" json:"id"`
+	Filename   string    `db:"filename" json:"filename"`
+	NoOptimize bool      `db:"no_optimize" json:"noOptimize"`
+	Size       int       `db:"size" json:"size"`
+	CreatedAt  time.Time `db:"created_at" json:"createdAt"`
 }
 
 func CreateImage(img *Image) error {
@@ -27,14 +24,11 @@ func CreateImage(img *Image) error {
 
 	_, err = conn.Exec(
 		ctx,
-		`INSERT INTO images (id, public_url, internal_path, no_optimize, size, group, tags, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
-		img.Id,
-		img.PublicUrl,
-		img.InternalPath,
+		`INSERT INTO images (id, filename, no_optimize, size, created_at) VALUES ($1, $2, $3, $4, $5, $6)`,
+		img.ID,
+		img.Filename,
 		img.NoOptimize,
 		img.Size,
-		img.Group,
-		img.Tags,
 		img.CreatedAt,
 	)
 	if err != nil {
@@ -56,16 +50,13 @@ func FindImageByID(id string) (*Image, error) {
 	var image Image
 	err = conn.QueryRow(
 		ctx,
-		`SELECT id, public_url, internal_path, no_optimize, size, group, tags, created_at FROM images WHERE id = $1`,
+		`SELECT id, filename, no_optimize, size, created_at FROM images WHERE id = $1`,
 		id,
 	).Scan(
-		&image.Id,
-		&image.PublicUrl,
-		&image.InternalPath,
+		&image.ID,
+		&image.Filename,
 		&image.NoOptimize,
 		&image.Size,
-		&image.Group,
-		&image.Tags,
 		&image.CreatedAt,
 	)
 	if err != nil {
@@ -86,7 +77,7 @@ func FindAllImages() ([]*Image, error) {
 
 	rows, err := conn.Query(
 		ctx,
-		`SELECT id, public_url, internal_path, no_optimize, size, group, tags, created_at FROM images`,
+		`SELECT id, filename, no_optimize, size, created_at FROM images`,
 	)
 	if err != nil {
 		return nil, err
@@ -97,13 +88,10 @@ func FindAllImages() ([]*Image, error) {
 	for rows.Next() {
 		var image Image
 		err = rows.Scan(
-			&image.Id,
-			&image.PublicUrl,
-			&image.InternalPath,
+			&image.ID,
+			&image.Filename,
 			&image.NoOptimize,
 			&image.Size,
-			&image.Group,
-			&image.Tags,
 			&image.CreatedAt,
 		)
 		if err != nil {
@@ -126,15 +114,12 @@ func UpdateImage(image *Image) error {
 
 	_, err = conn.Exec(
 		ctx,
-		`UPDATE images SET public_url = $1, internal_path = $2, no_optimize = $3, size = $4, group = $5, tags = $6, created_at = $7 WHERE id = $8`,
-		image.PublicUrl,
-		image.InternalPath,
+		`UPDATE images SET filename = $1, no_optimize = $2, size = $3, created_at = $4 WHERE id = $5`,
+		image.Filename,
 		image.NoOptimize,
 		image.Size,
-		image.Group,
-		image.Tags,
 		image.CreatedAt,
-		image.Id,
+		image.ID,
 	)
 
 	if err != nil {
