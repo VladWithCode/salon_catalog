e7bfbeccdc5c5ead7ecdc4e144381656d297e280
vladwb <vladwithcode@gmail.com>
2025-07-02 23:32:28 -0600

    chore: remove test routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,41 +3,24 @@ package routes
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
-	"github.com/vladwithcode/salon_catalog/internal/templates"
 )
 
 func NewRouter() http.Handler {
 	router := NewCustomServeMux()
 
 	router.HandleFunc("GET /{$}", RenderIndex)
-	router.HandleFunc("GET /test", TestFn)
 
 	return router
 }
 
 func RenderIndex(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "world"
-	}
-	response := fmt.Sprintf("Hello, %s!", name)
-	w.Write([]byte(response))
-}
-
-func TestFn(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "world"
-	}
-	t := templates.Demo(name)
-
-	err := t.Render(context.Background(), w)
+	err := pages.Index().Render(context.Background(), w)
 	if err != nil {
 		w.WriteHeader(500)
-		w.Write([]byte("failed to render template"))
-		fmt.Printf("err: %v\n", err)
+		w.Write([]byte("Something went wrong"))
+		log.Printf("failed to render Index err: %v\n", err)
 	}
 }
+
