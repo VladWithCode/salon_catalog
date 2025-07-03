3255df4dea605df8de532abfd80aff43d3f02fd8
vladwb <vladwithcode@gmail.com>
2025-07-02 23:28:19 -0600

    feat: file upload

---
+++ internal/uploads/uploads.go
@@ -0,0 +1,76 @@
+// Package uploads provides functions for uploading files
+package uploads
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var (
+	ErrFileHeaderOpenFail = errors.New("failed to open file from fileheader")
+	ErrFileCreateFail     = errors.New("failed to create output file")
+	ErrFileCopyFail       = errors.New("failed to copy file")
+
+	UploadsPath = os.Getenv("UPLOADS_PATH")
+)
+
+func writeFile(file multipart.FileHeader, writePath string) error {
+	p, err := file.Open()
+	if err != nil {
+		return errors.Join(ErrFileHeaderOpenFail, err)
+	}
+	defer p.Close()
+
+	outFile, err := os.Create(writePath)
+	if err != nil {
+		return errors.Join(ErrFileCreateFail, err)
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, p)
+	if err != nil {
+		return errors.Join(ErrFileCopyFail, err)
+	}
+
+	return nil
+}
+
+func Upload(file multipart.FileHeader) (filename string, err error) {
+	date := time.Now().Format("2006-01-02T15:04:05")
+	uploadsPath := filepath.Join("web/static/uploads")
+
+	filename = fmt.Sprintf("upload_%s_%d%s", date, 0, filepath.Ext(file.Filename))
+	writePath := filepath.Join(uploadsPath, filename)
+	err = writeFile(file, writePath)
+	if err != nil {
+		return "", err
+	}
+
+	return filename, nil
+}
+
+func UploadMultiple(files []multipart.FileHeader) (fileNames []string, err error) {
+	date := time.Now().Format("2006-01-02T15:04:05")
+	uploadsPath := filepath.Join("web/static/uploads")
+	fileNames = make([]string, len(files))
+
+	i := 0
+	for _, fHeader := range files {
+		filename := fmt.Sprintf("upload_%s_%d%s", date, i, filepath.Ext(fHeader.Filename))
+		writePath := filepath.Join(uploadsPath, filename)
+
+		err = writeFile(fHeader, writePath)
+		if err != nil {
+			return nil, err
+		}
+
+		fileNames[i] = filename
+	}
+
+	return fileNames, nil
+}
