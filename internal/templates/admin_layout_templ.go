bbde459d0c7c49bc0546f72cf7cc272b5af06d0d
vladwb <vladwithcode@gmail.com>
2025-08-01 12:10:06 -0600

    fix: invisible sidebar for large screens

--- internal/templates/admin_layout_templ.go
+++ internal/templates/admin_layout_templ.go
@@ -104,7 +104,7 @@ func AdminLayout(title string, headerTitle string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "</head><body><div class=\"flex h-screen w-screen overflow-hidden z-0 p-0.5\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "</head><body><div class=\"flex h-screen w-screen overflow-hidden xl:flex gap-1 z-0 p-0.5\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
