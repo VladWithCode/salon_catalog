68142d0c7ba9f5d8f93b5014f1c36efc8bc7cd0d
vladwb <vladwithcode@gmail.com>
2025-08-05 18:41:22 -0600

    fix: descriptions exceeding column size

--- sql/migrations/20250703200230_add_categories_table.sql
+++ sql/migrations/20250703200230_add_categories_table.sql
@@ -2,9 +2,9 @@
 -- +goose StatementBegin
 CREATE TABLE categories (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(120) NOT NULL,
+    name VARCHAR(200) NOT NULL,
     slug VARCHAR(200) UNIQUE NOT NULL,
-    description VARCHAR(128) NOT NULL,
+    description VARCHAR(360) NOT NULL,
     long_description VARCHAR(512),
     header_img UUID REFERENCES images(id) ON DELETE SET NULL,
     display_img UUID REFERENCES images(id) ON DELETE SET NULL
